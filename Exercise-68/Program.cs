944577552d72373316372f07a34222e6866bfc11
Art <artemgoryunov89@gmail.com>
2023-08-31 20:12:10 +0400

    Exercise-68

---
+++ Exercise-68/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите неотрицательное число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите неотрицательное число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 <0 || number2<0 )
+{
+    Console.WriteLine("Вы ввели некорректные числа");
+    return;
+}
+
+int Ack(int n, int m)
+{
+    if (n == 0)
+    {
+        return m+1;
+    }
+    else if (m==0)
+    {
+        return Ack(n-1,1);
+    }
+    else
+    {
+          return Ack(n-1,Ack(n,m-1));
+    }
+}
+
+Console.WriteLine(Ack(number1,number2));
