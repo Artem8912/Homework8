86cb38d42a68b9f1e5a8850b4da7e89e72fa930a
Art <artemgoryunov89@gmail.com>
2023-08-31 20:09:59 +0400

    Exercise-66

---
+++ Exercise-66(1)/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите целое число №1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число №2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 <= 0 && number2 <= 0)
+{
+    Console.WriteLine("Сумму посчитать невозможно");
+    return;
+}
+else if (number1 > number2 && number2 <= 0)
+{
+    while (number2 <= 0)
+    {
+        number2++;
+    }
+
+}
+else
+{
+    while (number1 <= 0)
+    {
+        number1++;
+    }
+}
+int SumOfNaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        return num2 + SumOfNaturalNumbers(num1, num2 + 1);
+    }
+    else if (num2 > num1)
+    {
+        return num1 + SumOfNaturalNumbers(num1 + 1, num2);
+    }
+
+    else
+    {
+        return num1;
+    }
+}
+Console.WriteLine("Сумма натуральных чисел равна: ");
+Console.WriteLine(SumOfNaturalNumbers(number1, number2));
