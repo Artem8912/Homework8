c295fbf254cbb76789051ba2b3a3c1eb34209fc8
Art <artemgoryunov89@gmail.com>
2023-08-31 20:08:50 +0400

    Exercise-64

---
+++ Exercise-64/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+    return;
+}
+
+void Numbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    Numbers(num - 1);
+
+}
+
+Numbers(number);
+
