d43d70a77986ef4120f5edfbf4ecc6d518a70f7a
Art <artemgoryunov89@gmail.com>
2023-08-31 20:14:09 +0400

    Exercise-68(1)

--- Exercise-68/Program.cs
+++ Exercise-68/Program.cs
@@ -4,7 +4,7 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите неотрицательное число 2: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 <0 || number2<0 )
+if (number1 < 0 || number2 < 0)
 {
     Console.WriteLine("Вы ввели некорректные числа");
     return;
@@ -14,16 +14,16 @@ int Ack(int n, int m)
 {
     if (n == 0)
     {
-        return m+1;
+        return m + 1;
     }
-    else if (m==0)
+    else if (m == 0)
     {
-        return Ack(n-1,1);
+        return Ack(n - 1, 1);
     }
     else
     {
-          return Ack(n-1,Ack(n,m-1));
+        return Ack(n - 1, Ack(n, m - 1));
     }
 }
 
-Console.WriteLine(Ack(number1,number2));
+Console.WriteLine(Ack(number1, number2));
